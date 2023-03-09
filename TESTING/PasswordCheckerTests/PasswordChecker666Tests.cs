30d4bfd0efc18a08dc2af534906aa6b36107916c
VorobevPavell <117814069+VorobevPavell@users.noreply.github.com>
2023-03-09 12:39:10 +0300

    Add files via upload

---
+++ TESTING/PasswordCheckerTests/PasswordChecker666Tests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker.Tests
+{
+    [TestClass()]
+    public class PasswordChecker666Tests
+    {
+        [TestMethod()]
+        public void Check_8Symbols_ReturnsTrue()
+        {
+            // Arrange
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            // Act
+            bool actual = PasswordChecker666.ValidatePassword(password);
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Check_PasswordWithoutLowerSymbols_ReturnsFalse()
+        {
+            // Arrange
+            string password = "ASDQWE123$";
+            bool expected = false;
+
+
+            // Act
+            bool actual = PasswordChecker666.ValidatePassword(password);
+
+
+            // Assert
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod()]
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            // Arrange
+            string password = "Aq1$";
+
+
+            // Act
+            bool actual = PasswordChecker666.ValidatePassword(password);
+
+
+            // Assert
+            Assert.IsFalse(actual);
+
+        }
+        [TestMethod()]
+        public void Check_PasswordWithoutLowerSymbols_ReturnsTrue()
+        {
+            // Arrange
+            string password = "ASDq123$";
+            bool expected = true;
+
+
+            // Act
+            bool actual = PasswordChecker666.ValidatePassword(password);
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
